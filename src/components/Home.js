63ede930a3199fa1173fac53a685b7c0acb2002f
Yeeun Jung <rjyeeun@gmail.com>
2023-01-08 23:53:48 -0500

    commit tree fd886bf7ee860241cba16d2cf74a4bed62dfa675

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const divStyle = {
+  color: "firebrick"
+}
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home"> 
+    <h1 style={divStyle}>"Your {name} is a Web Developer from Your {city}"</h1>
+  </div>;
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  let element =<a href="#home">Home</a>
+  let about = <a href="#about">About</a>
+  return <nav> 
+    {element}
+    {about}
+    </nav>;
 }
 
 export default NavBar;
